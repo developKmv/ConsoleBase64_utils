9591232a95265b09aad84eeff0dce568d5028d27
mvx <maxrprp@gmail.com>
2021-05-20 23:36:15 +0300

    second commit

--- ConsoleBase64_utils/ConsoleBase64_utils.cpp
+++ ConsoleBase64_utils/ConsoleBase64_utils.cpp
@@ -20,9 +20,10 @@ string base64_encode(std::uint8_t const* data, std::size_t len)
 
 
 
-int main()
+
+int main(int argc, char* argv[])
 {
-    std::ifstream is("d:/Develop/Cpp/tmp/test_x.ods", std::ifstream::binary);
+    std::ifstream is(argv[1], std::ifstream::binary);
     if (is) {
         // get length of file:
         is.seekg(0, is.end);
@@ -43,16 +44,13 @@ int main()
 
         // ...buffer contains the entire file...
         //cout << "\n buff \n"<< buffer;
-        cout << '\n' << base64_encode(reinterpret_cast<uint8_t*>(buffer), length);
+
+        ofstream outf("output_to_base64.txt");
+        outf << base64_encode(reinterpret_cast<uint8_t*>(buffer), length);
+
+        std::cout << "\nall characters write successfully.";
+
         delete[] buffer;
     }
 
-    /*const boost::interprocess::mode_t mode = boost::interprocess::read_only;
-    boost::interprocess::file_mapping fm("d:/Develop/Cpp/tmp/test.txt ", mode);
-    boost::interprocess::mapped_region region(fm, mode, 0, 0);
-    const char* begin = static_cast<const char*>(region.get_address());
-    const char* pos = std::find(begin, begin + region.get_size(), '\1');*/
-    
-    /*uint8_t dataArray[] = {1,2,3,4};
-    cout << base64_encode(dataArray, 4);*/
 }
